08a96ac13e8516d03655d54c9d8542fe307ace2f
Joseph Karanja <joseph.karanja@student.moringaschool.com>
2023-06-14 21:16:57 +0300

    complete assignement

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+function multiply(num1, num2){
+    return num1 * num2
+}
 
-// Write your code here
+console.log(multiply(31,2))
+
+function random(randomnumber){
+    return Math.floor(Math.random()* 100) + 1;
+}
+
+console.log(random())
+
+function mod(num3, num4){
+    return num4 / num3;
+}
+
+console.log(mod(5, 20))
+
+function max(highest){
+    return Math.max(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+}
+
+console.log(max())
+
